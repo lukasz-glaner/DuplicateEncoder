7abd7b72de23c1372f3673dc47f7976a3abe6561
lukasz-glaner <lukasz.glaner@gmail.com>
2022-11-30 15:54:05 +0100

    add readme description

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         System.out.println(encode("Mamamakota"));
-        
+
     }
 
     static String encode(String word) {
